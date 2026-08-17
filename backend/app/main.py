09b1d191bcbc3b9ccd49dbff1cbb2cbabd3d3ff8
ayushartistic <114128057+ayushartistic@users.noreply.github.com>
2026-08-17 07:47:38 +0530

    Add Frontend origin in backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -96,6 +96,7 @@ app.add_middleware(
     "http://127.0.0.1:5173",
     "http://localhost:5174",
     "http://127.0.0.1:5174",
+    "https://trustlens-v2.vercel.app"
 ],
     allow_credentials=True,
     allow_methods=["*"],
